55b3acaa47a4d09657027397dc7af5cdf121f92c
rjirwanshah rjhassan <rjirwanshah@rjirwanshah.local>
2016-09-03 15:57:55 +0800

    button and textview

--- app/src/main/java/example/my/hellolayout/HelloFragment.java
+++ app/src/main/java/example/my/hellolayout/HelloFragment.java
@@ -6,11 +6,19 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 /**
  * Created by rjirwanshah on 03/09/2016.
  */
 public class HelloFragment extends Fragment {
+
+    private TextView mMessageTextView;
+    private Button mHelloButton;
+    private Button mByeButton;
 
     public HelloFragment() {
     }
@@ -20,6 +28,37 @@ public class HelloFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         //return super.onCreateView(inflater, container, savedInstanceState);
         View rootView = inflater.inflate(R.layout.hello_fragment, container, false);
+
+        mMessageTextView = (TextView) rootView.findViewById(R.id.hello_fragment_tv_message);
+        mHelloButton = (Button) rootView.findViewById(R.id.button_hello);
+        mByeButton = (Button) rootView.findViewById(R.id.button_bye);
+
         return rootView;
     }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        mHelloButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mMessageTextView.setText("Hello there!");
+            }
+        });
+
+        mByeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mMessageTextView.setText("Bye!");
+            }
+        });
+    }
+
+//    @Override
+//    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+//        super.onActivityCreated(savedInstanceState);
+//
+//        mMessageTextView.setText("Hello World");
+//    }
 }
